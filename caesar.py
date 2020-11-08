16b34338b7b52fdefa9bc0aab75b1064d9c3ded7
2001houpe <71071656+2001houpe@users.noreply.github.com>
2020-11-08 11:59:23 +0100

    Úkol

--- caesar.py
+++ caesar.py
@@ -3,40 +3,28 @@
 # Datum:   02.10.2018 00:36
 # Autor:   Marek Nožka, nozka <@t> spseol <d.t> cz
 ############################################################################
-import unicodedata
-from sys import argv, stdin, stdout, stderr
+
+soubor = open('sifra.txt', encoding = 'utf-8')
+
+obsah = soubor.read()
+
+soubor.close()
 
 
-PISMENA = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-try:
-    KLIC = int(argv[1])
-except IndexError:
-    stderr.write(argv[0])
-    stderr.write(': CHYBA: Jak parametr musíš zadat klíč!\n\n')
-    exit(2)
-except ValueError:
-    stderr.write(argv[0])
-    stderr.write(': CHYBA: Klíč je celé číslo!.\n\n')
-    exit(3)
+def caesar():
+    key = int(input("Zadejte klíč:"))
+    plaintext = obsah
+    zprava = ""
+    for pismeno in plaintext:
+        prevod=ord(pismeno)
+        prevod=prevod + key
+        if (prevod > ord("Z")):
+            prevod = prevod - 26
+        pismeno=chr(prevod)
+        zprava = zprava + pismeno
+    print(zprava)
 
-if stdin.isatty():
-    print('Zadej text a ukonči ho Ctrl+D. (Ve Widlích Ctrl+Z)')
-text = stdin.read()
+caesar()
 
-text = ''.join(text.split())
-text = unicodedata.normalize('NFD', text).upper()\
-        .encode('ascii', 'ignore').decode('ascii')
 
-if len(argv) > 2:
-    print()
-    print(text)
-    print()
-
-for c in text:
-    try:
-        i = PISMENA.index(c)
-    except ValueError:
-        continue
-    stdout.write(PISMENA[(i + KLIC) % len(PISMENA)])
-print()
